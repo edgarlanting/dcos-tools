dc2345ef2cc8e00b13f213ae25c7fead9fdcd605
Shredgar <edgarlanting@users.noreply.github.com>
2018-06-20 11:35:41 +0200

    Create dcos-fix-fbs.py
    Added initial commit of the dcos-fix-fbs.py script to fix broken deployments caused by file based secrets. This script runs a group update to all broken apps via a PUT towards the API endpoint.

---
+++ dcos-fix-fbs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+import json
+import requests
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def get_dcos_url_from_cli():
+    res = subprocess.run(["dcos", "config", "show", "core.dcos_url"], shell=False, stdout=subprocess.PIPE)
+    return res.stdout.strip().decode("utf-8")
+
+
+def get_dcos_token_from_cli():
+    res = subprocess.run(["dcos", "config", "show", "core.dcos_acs_token"], shell=False, stdout=subprocess.PIPE)
+    return res.stdout.strip().decode("utf-8")
+
+
+if __name__ == "__main__":
+    url = get_dcos_url_from_cli()
+    token = get_dcos_token_from_cli()
+    combined_json = list()
+    for app_filename in sys.argv[1:]:
+        with open(app_filename) as app_file:
+            data = app_file.read()
+            app_def = json.loads(data)
+            combined_json.append(app_def)
+    response = requests.put(url + "/service/marathon/v2/apps", json=combined_json, headers={"Authorization": "token=%s" % token}, verify=False)
+    if not response.ok:
+        print(response.text)
+    print(response.json())
